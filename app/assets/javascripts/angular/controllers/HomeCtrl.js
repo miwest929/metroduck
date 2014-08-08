bde4890d2de608f2dfe6bce37caf7f898c6ef329
Michael West <mwest@mdsol.com>
2014-08-08 12:16:05 -0400

    Fix some bugs. Include before changes for better context

--- app/assets/javascripts/angular/controllers/HomeCtrl.js
+++ app/assets/javascripts/angular/controllers/HomeCtrl.js
@@ -1,41 +1,26 @@
 deploy_guides.controller('HomeCtrl', ['$scope', 'deployGuideService', function($scope, Guide) {
-  var isChangeInNewBranch = function(diff_line) {
-    return (diff_line.charAt(0) === ">");
-  }
-
-  var isChangeInOldBranch = function(diff_line) {
-    return (diff_line.charAt(0) === "<");
-  }
-
-  var filterOut = function(lines, filterFn) {
-    var filteredLines = [];
-
-    for (index = 0; index < lines.length; ++index) {
-      if (!filterFn(lines[index])) {
-        filteredLines.push(lines[index]);
-      }
-    }
-
-    return filteredLines;
-  }
-
-  // selectFn must return true or false. true implies that the line should get be included.
-  var selectLines = function(lines, selectFn) {
-    var selectedLines = [];
-
-    for (index = 0; index < lines.length; ++index) {
-      if (selectFn(lines[index])) {
-        selectedLines.push(lines[index]);
-      }
-    }
-
-    return selectedLines;
-  }
-
   var parseDiffOutput = function(diff_output) {
-    var lines = diff_output.split("\n");
 
-    var relevantLines = filterOut(lines, isChangeInOldBranch);
+    var isChangeInNewBranch = function(diff_line) {
+      return (diff_line.charAt(0) === ">");
+    }
+
+    var isChangedLine = function(diff_line) {
+      return (isChangeInNewBranch(diff_line) || isChangeInOldBranch(diff_line) || isDiffFragment(diff_line));
+    }
+
+    var isChangeInOldBranch = function(diff_line) {
+      return (diff_line.charAt(0) === "<");
+    }
+
+    var isDiffFragment = function(line) {
+      regexp = new RegExp(/([1-9]+[acd]{1})+/);
+
+      return (regexp.exec(line) !== null);
+    }
+
+    var lines = diff_output.split("\n");
+    relevantLines = lines.filter(isChangedLine);
 
     // Strip out trailing '>'
     for (index = 0; index < relevantLines.length; index++) {
@@ -44,21 +29,18 @@ deploy_guides.controller('HomeCtrl', ['$scope', 'deployGuideService', function($
       }
     }
 
-    var isDiffFragment = function(line) {
-      regexp = new RegExp(/[1-9ad,]+/);
-
-      return regexp.exec(line) === null;
-    }
-
     var indexFragmentStart = -1;
     var fragments = [];
     for(index = 0; index < relevantLines.length; index++) {
       if (isDiffFragment(relevantLines[index])) {
-        if (indexFragmentStart !== -1 && index > indexFragmentStart) {
-          fragments.push({
-            content: relevantLines.slice(indexFragmentStart, index - 1).join("\n"),
-            isSelected: false
-          });
+        if (index > indexFragmentStart) {
+          var fragmentContent = relevantLines.slice(indexFragmentStart, index).join("\n");
+          if ( $.trim(fragmentContent) !== '' ) {
+            fragments.push({
+              content: fragmentContent,
+              isSelected: false
+            });
+          }
         }
         indexFragmentStart = index + 1;
       }
@@ -67,7 +49,6 @@ deploy_guides.controller('HomeCtrl', ['$scope', 'deployGuideService', function($
     return fragments;
   };
 
-  $scope.diff_output = null;
   $scope.in_construct_state = false;
 
   $scope.generateDeployGuide = function() {
@@ -94,12 +75,12 @@ deploy_guides.controller('HomeCtrl', ['$scope', 'deployGuideService', function($
     console.log("Extracting deploy guide for repo: " + $scope.repo.name);
     Guide.getInstallationGuide($scope.repo.name, $scope.repo.old_branch, $scope.repo.new_branch, function(data, status) {
       if (status === 422) {
-        console.log("422!!!");
-        $scope.diff_output = data.error;
+        $scope.deployGuide = data.error;
+      } else if(status === 500) {
+        $scope.deployGuide = "Server threw up all over our request :("
       } else {
         $scope.in_construct_state = true;
         $scope.fragments = parseDiffOutput(data.contents);
-        $scope.diff_output = parseDiffOutput(data.contents);
       }
     });
   }
