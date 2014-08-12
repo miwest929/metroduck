de96dd9e639151d1e327b80e757f801e69a98bae
Michael West <mwest@mdsol.com>
2014-08-12 09:15:18 -0400

    Make generated deploy guide editable

--- app/assets/javascripts/angular/controllers/HomeCtrl.js
+++ app/assets/javascripts/angular/controllers/HomeCtrl.js
@@ -1,6 +1,7 @@
 deploy_guides.controller('HomeCtrl', ['$scope', 'deployGuideService', function($scope, Guide) {
-  var parseDiffOutput = function(diff_output) {
+  var ignoreFragmentRules = [];
 
+  var parseDiffOutput = function(diff_output) {
     var isChangeInNewBranch = function(diff_line) {
       return (diff_line.charAt(0) === ">");
     }
@@ -19,37 +20,47 @@ deploy_guides.controller('HomeCtrl', ['$scope', 'deployGuideService', function($
       return (regexp.exec(line) !== null);
     }
 
+    var removeFirstChar = function(line) {
+      return line.substr(1);
+    }
+
+    var computeFragments = function(lines) {
+      var indexFragmentStart = -1;
+      var fragments = [];
+      for(index = 0; index < lines.length; index++) {
+        if (isDiffFragment(lines[index]) && (index > indexFragmentStart)) {
+          if (index > indexFragmentStart) {
+            var fragmentContent = lines.slice(indexFragmentStart, index)
+            if ( $.trim(fragmentContent) !== '' ) {
+              oldChanges = fragmentContent.filter(isChangeInOldBranch).map(removeFirstChar);
+              newChanges = fragmentContent.filter(isChangeInNewBranch).map(removeFirstChar);
+
+              fragments.push({
+                old_changes: oldChanges.join("\n"),
+                new_changes: newChanges.join("\n"),
+                isSelected: false
+              });
+            }
+          }
+          indexFragmentStart = index + 1;
+        }
+      }
+
+      return fragments;
+    };
+
     var lines = diff_output.split("\n");
     relevantLines = lines.filter(isChangedLine);
 
-    // Strip out trailing '>'
-    for (index = 0; index < relevantLines.length; index++) {
-      if (isChangeInNewBranch(relevantLines[index])) {
-        relevantLines[index] = relevantLines[index].substr(1);
-      }
-    }
+    fragments = computeFragments(relevantLines);
 
-    var indexFragmentStart = -1;
-    var fragments = [];
-    for(index = 0; index < relevantLines.length; index++) {
-      if (isDiffFragment(relevantLines[index])) {
-        if (index > indexFragmentStart) {
-          var fragmentContent = relevantLines.slice(indexFragmentStart, index).join("\n");
-          if ( $.trim(fragmentContent) !== '' ) {
-            fragments.push({
-              content: fragmentContent,
-              isSelected: false
-            });
-          }
-        }
-        indexFragmentStart = index + 1;
-      }
-    }
+    // Remove fragments that are whitespace changes only
 
     return fragments;
   };
 
   $scope.in_construct_state = false;
+  $scope.deployGuide = null;
 
   $scope.generateDeployGuide = function() {
     $scope.in_construct_state = false;
@@ -57,12 +68,14 @@ deploy_guides.controller('HomeCtrl', ['$scope', 'deployGuideService', function($
     wantedFragments = [];
     for(index = 0; index < $scope.fragments.length; index++) {
       if ($scope.fragments[index].isSelected) {
-        wantedFragments.push($scope.fragments[index].content);
+        wantedFragments.push($scope.fragments[index].new_changes);
       }
     }
 
-    $scope.deployGuide = wantedFragments.join("\n");
-    console.log($scope.deployGuide);
+    if (wantedFragments.length === 0)
+      $scope.deployGuide = "Generated deployment guide is empty.";
+    else
+      $scope.deployGuide = wantedFragments.join("\n");
   }
 
   $scope.inConstructState = function() {
@@ -70,17 +83,19 @@ deploy_guides.controller('HomeCtrl', ['$scope', 'deployGuideService', function($
   }
 
   $scope.computeDeployGuide = function() {
-    $scope.in_construct_state = false;
-
     console.log("Extracting deploy guide for repo: " + $scope.repo.name);
     Guide.getInstallationGuide($scope.repo.name, $scope.repo.old_branch, $scope.repo.new_branch, function(data, status) {
+      $scope.in_construct_state = false;
       if (status === 422) {
         $scope.deployGuide = data.error;
       } else if(status === 500) {
         $scope.deployGuide = "Server threw up all over our request :("
       } else {
-        $scope.in_construct_state = true;
         $scope.fragments = parseDiffOutput(data.contents);
+        if ($scope.fragments.length === 0)
+          $scope.deployGuide = "No infrastructure changes detected.";
+        else
+          $scope.in_construct_state = true;
       }
     });
   }
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,27 @@
 var deploy_guides = angular.module('extract-deploy-guides', ['ngRoute', 'ngResource'])
-  //.controller('HomeCtrl', [
 
 deploy_guides.config(['$routeProvider', function ($routeProvider) {
   $routeProvider.otherwise({
     templateUrl: '/templates/home.html',
     controller: 'HomeCtrl'
   })
-}])
+}]);
+
+deploy_guides.directive('contenteditable', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, element, attrs, ctrl) {
+      // view -> model
+      element.bind('blur', function() {
+        scope.$apply(function() {
+          ctrl.$setViewValue(element.html());
+        });
+      });
+
+      // model -> view
+      ctrl.$render = function() {
+        element.html(ctrl.$viewValue);
+      };
+    }
+  };
+});
